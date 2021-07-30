942707dae447271607fe9577a1782fa52c09d82a
lukakap <lkapa18@freeuni.edi.ge>
2021-07-30 10:32:36 +0400

    RequestDAO initialization start

---
+++ src/main/java/freeuni/edu/ge/DAO/RequestDAO.java
@@ -0,0 +1,28 @@
+package freeuni.edu.ge.DAO;
+
+import freeuni.edu.ge.Models.Request;
+
+import java.util.Iterator;
+
+public class RequestDAO {
+
+    public void addNewDoctorRegistrationRequest(String name, String surname, String ID){
+
+    }
+
+    public Iterator<Request> getIterator(){
+        return null;
+    }
+
+    public void requestAnswer(boolean answer, String ID){
+
+    }
+
+    private void removeFromRequest(String ID){
+
+    }
+
+    private void removeAndAddToCanRegisterTable(String ID){
+
+    }
+}
---
+++ src/test/java/RequestDAOTest.java
@@ -0,0 +1,10 @@
+public class RequestDAOTest {
+
+
+
+
+    public void simple1(){
+
+
+    }
+}
