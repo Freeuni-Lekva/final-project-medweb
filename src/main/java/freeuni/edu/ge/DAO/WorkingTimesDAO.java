1c8280036d8d314d140f0922b4f974f361bdd7e6
TornikeG-G <tgvan18@freeuni.edu.ge>
2021-08-12 14:08:58 +0400

    Changed AddDoctor Method.

--- src/main/java/freeuni/edu/ge/DAO/WorkingTimesDAO.java
+++ src/main/java/freeuni/edu/ge/DAO/WorkingTimesDAO.java
@@ -11,23 +11,22 @@ public class WorkingTimesDAO implements  WorkingTimesDAOInterface{
     private static final int NEXT_DAYS = 8;
     private static final int WORK_START_TIME = 10;
 
-    @Override
+
     public void addDoctor(Doctor doctor) {
         LocalDateTime localDateTime = LocalDateTime.now();
         Date now = new Date(localDateTime.getYear(), localDateTime.getMonthValue(), localDateTime.getDayOfMonth());
-        List<Time> dayGraphic = makeDayGraphic();
-        Map<Date, List<Time> > dateAndTime = makeDateAndTime(now, dayGraphic);
+        Map<Date, List<Time> > dateAndTime = makeDateAndTime(now);
         reservedDoctors.put(doctor.getID(), dateAndTime);
     }
     /**
      *
      * @param date current date.
-     * @param dayGraphic method makeDayGraphic();
      * @return map, where Keys are next 7 days and values each days working times introduce in arraylist.
      */
-    private Map<Date, List<Time>> makeDateAndTime(Date date, List<Time> dayGraphic) {
+    private static Map<Date, List<Time>> makeDateAndTime(Date date) {
         Map<Date, List<Time> > dateAndTime = new HashMap<>();
         for(int days=0; days < NEXT_DAYS ;days ++) {
+            List<Time> dayGraphic = makeDayGraphic();
             Date nextDate = new Date(date.getYear(), date.getMonth()-1, date.getDate()+days);
             dateAndTime.put(nextDate,dayGraphic);
         }
@@ -38,7 +37,7 @@ public class WorkingTimesDAO implements  WorkingTimesDAOInterface{
      *
      * @return each day working graphic times.
      * */
-    private List<Time> makeDayGraphic() {
+    private static List<Time> makeDayGraphic() {
         List<Time> dayGraphic = new ArrayList<>();
         int minute = 0;
         int second = 0;
