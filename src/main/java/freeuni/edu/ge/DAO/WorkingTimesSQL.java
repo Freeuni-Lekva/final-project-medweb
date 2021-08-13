1b9eca08c1ac00498d25a22cfb8c08599af81ea2
TornikeG-G <tgvan18@freeuni.edu.ge>
2021-08-13 18:30:23 +0400

    Added SQL Base Structure in Txt File

--- src/main/java/freeuni/edu/ge/DAO/WorkingTimesSQL.java
+++ src/main/java/freeuni/edu/ge/DAO/WorkingTimesSQL.java
@@ -1,12 +1,20 @@
 package freeuni.edu.ge.DAO;
 
 import freeuni.edu.ge.Models.Doctor;
+import org.apache.commons.dbcp2.BasicDataSource;
 
 import java.sql.SQLException;
 import java.sql.Time;
 import java.util.Date;
 
 public class WorkingTimesSQL implements WorkingTimesDAOInterface{
+
+    private BasicDataSource source;
+
+    public WorkingTimesSQL (BasicDataSource source){
+        this.source = source;
+    }
+
     @Override
     public void addDoctor(Doctor doctor) throws SQLException {
 
