31182441e8c9b2db549e1a8e9507b36b4e47e81c
TornikeG-G <tgvan18@freeuni.edu.ge>
2021-08-13 17:40:31 +0400

    Added WorkingTimesSQL, which implements WorkingTimesDAOINTERFACE

---
+++ src/main/java/freeuni/edu/ge/DAO/WorkingTimesSQL.java
@@ -0,0 +1,24 @@
+package freeuni.edu.ge.DAO;
+
+import freeuni.edu.ge.Models.Doctor;
+
+import java.sql.SQLException;
+import java.sql.Time;
+import java.util.Date;
+
+public class WorkingTimesSQL implements WorkingTimesDAOInterface{
+    @Override
+    public void addDoctor(Doctor doctor) throws SQLException {
+
+    }
+
+    @Override
+    public void updateBase() {
+
+    }
+
+    @Override
+    public void reserveDoctor(Doctor doctor, Date date, Time time) {
+
+    }
+}
