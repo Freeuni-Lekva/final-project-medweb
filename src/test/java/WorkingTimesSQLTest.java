0bc155d5865b36087d1b318df086f50b17a3ffbd
TornikeG-G <tgvan18@freeuni.edu.ge>
2021-08-15 16:55:01 +0400

    Added WorkingTimesSQL Test clas

---
+++ src/test/java/WorkingTimesSQLTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+public class WorkingTimesSQLTest {
+    @BeforeEach
+    public void init() {
+
+    }
+
+    @Test
+    public void FirstTest(){
+
+    }
+}
